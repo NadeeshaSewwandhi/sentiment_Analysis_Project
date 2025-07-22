e64606b410729e24ed68f0552455d18ee5950a43
Nadeesha Sewwandhi <nadeeshasewwandhi00@gmail.com>
2025-07-22 23:15:22 +0530

    logger file created

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, redirect
 from numpy import vectorize
 from prediction_pipeline import preprocessing, get_prediction, vectorize_text
-
+from my_logger import logging
 
 
 app = Flask(__name__)
+
+logging.info('Flask server started')
 
 data = dict()
 reviews = []
@@ -16,15 +18,24 @@ def index():
     data['reviews'] = reviews
     data['positive'] = positive
     data['negative'] = negative
+
+    logging.info('============home page ============')
     return render_template('index.html', data= data)
     
 
 @app.route("/" , methods = ['post'])
 def my_post():
     text= request.form['text']
+    logging.info(f'Text : {text}')
+
     preprocessed_txt = preprocessing(text)
+    logging.info(f'Preprocessed Text : {preprocessed_txt}')
+
     vectorized_txt = vectorize_text(preprocessed_txt )
+    logging.info(f'vectorized Text : {vectorized_txt}')
+
     prediction = get_prediction(vectorized_txt)
+    logging.info(f'Prediction : {prediction}')
 
     if prediction == 'negtive':
         global negative
